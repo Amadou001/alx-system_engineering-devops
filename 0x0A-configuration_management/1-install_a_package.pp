b6212c9482331a47ffe70bd3eb707ad2d9b17f98
Amadou001 <bamadou634@gmail.com>
2024-06-29 00:25:24 +0800

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,10 @@
-# install flask version 2.1.0
-exec{ 'flask':
-command  => 'pip3 install flask==2.1.0',   # command to install flask
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0', # Command to install Flask
+  require => Package['python3-pip'], # Ensure this runs after pip3 is installed
 }
