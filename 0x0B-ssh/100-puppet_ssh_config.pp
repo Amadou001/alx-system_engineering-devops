f8172b6fc580d38799eb1f5f9b9e38ba47d7417c
Amadou001 <bamadou634@gmail.com>
2024-06-30 00:47:10 +0800

    SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,7 @@
 # Ensure SSH client configuration file exists
 file { '/home/ubuntu/.ssh/config':  # Adjust path as necessary
   ensure  => file,
-  mode    => '0600',  # Set permissions to 600
-  owner   => 'ubuntu',  # Replace with appropriate user
-  group   => 'ubuntu',  # Replace with appropriate group
   content => "
-    # Global options (applies to all hosts unless overridden)
-    Host *
-        ServerAliveInterval 60
-        ForwardX11 no
-    
     # Configuration for specific host
         HostName 54.144.139.236  # Replace with your server's IP address or hostname
         User ubuntu               # Replace with your remote username
