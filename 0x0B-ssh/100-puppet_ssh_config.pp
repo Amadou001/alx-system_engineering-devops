7e3fad401a8e456a397e56785be3d241a1563eef
Amadou001 <bamadou634@gmail.com>
2024-06-30 00:55:05 +0800

    SSH puppet configuration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,15 @@
-# Ensure SSH client configuration file exists
-file { '~/.ssh/config':  # Adjust path as necessary
-  ensure  => file,
-  content => "
-    # Configuration for specific host
-        HostName 54.144.139.236  # Replace with your server's IP address or hostname
-        User ubuntu               # Replace with your remote username
-        IdentityFile ~/.ssh/school
-        PasswordAuthentication no
-  ",
+# Configure config file using pupet
+
+include stdlib
+
+file_line { 'Refuse to authenticate using a password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
 }
